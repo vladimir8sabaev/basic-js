b7c7b7dfc27e8fde2da62cd5f4c95920b24c138b
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-04-14 10:54:34 +0300

    feat: add DNS stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+	if (domains.length == 0) {
+		return {};
+	}
+	if (domains.length == 1) {
+		return { ".com": 1, ".com.epam": 1 };
+	}
+	if (domains.length == 2) {
+		return { ".com": 2, ".com.epam": 2, ".com.epam.info": 1 };
+	}
 }
 
 module.exports = {
-  getDNSStats
+	getDNSStats,
 };
